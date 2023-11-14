dc0fbf1b1d92d0467de0c7c2f423bbcba16b5f02
Mizu <psdk55052004@gmail.com>
2023-11-14 14:23:56 +0800

    新增 axios

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
 <template>
-  123
+  <p>{{ result }}</p>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import axios from 'axios'
 
+// data
+const result = ref()
+
+getAjax()
+
+async function getAjax () {
+  const config = {
+    msthod: 'get',
+    url: 'https://randomuser.me/api/',
+    data: {}
+  }
+  try {
+    const res = await axios(config)
+    console.log(res)
+    result.value = res.data.info.seed
+
+    console.log(result.value)
+  } catch (err) {
+    console.error(err)
+  }
+}
 </script>
 
 <style lang='scss' scope></style>
