4260a9696f1b2fbe1e4b48a71ae2bf739b18b7c4
Mizu <psdk55052004@gmail.com>
2023-11-14 14:07:53 +0800

    新增 vue-router

--- src/main.js
+++ src/main.js
@@ -1,33 +1,12 @@
 import { createApp } from 'vue'
+import router from './router'
 
-// import './style.css'
 import 'bootstrap'
-
-// import { defineConfig } from 'vite';
-
-// export default defineConfig({
-//   resolve: {
-//     alias: [
-//         {
-//             // this is required for the SCSS modules
-//             find: /^~(.*)$/,
-//             replacement: '$1',
-//         },
-//     ],
-// },
-//   // base: '/my-app/',
-//   // build: {
-//   //   outDir: 'dist',
-//   //   assetsDir: 'assets',
-//   //   // 其他構建選項...
-//   // },
-//   // server: {
-//   //   port: 3000,
-//   //   // 其他伺服器配置...
-//   // },
-//   // // 其他項目配置...
-// });
 
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  123
+</template>
+
+<script setup>
+
+</script>
+
+<style lang='scss' scope></style>
